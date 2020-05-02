0efa4b31e315db8c4f215fc6d23021bfca42e19e
Vladimir Goldobin <bobagold@gmail.com>
2020-05-02 18:23:02 +0200

    fix TapSkin states

--- lib/skins/text_tap_skin.dart
+++ lib/skins/text_tap_skin.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 /// Text tap skin for the game board
-class TextTapSkin extends StatefulWidget {
+class TextTapSkin extends StatelessWidget {
   final TextEditingController controller;
   final VoidCallback onChanged;
   final bool readOnly;
@@ -16,30 +16,25 @@ class TextTapSkin extends StatefulWidget {
   }) : super(key: key);
 
   @override
-  State<StatefulWidget> createState() {
-    return _TextTapSkinState();
-  }
-}
-
-class _TextTapSkinState extends State<TextTapSkin> {
-  @override
   Widget build(BuildContext context) => InkWell(
-        onTap: widget.readOnly ? null : _onTap,
+        onTap: readOnly ? null : _onTap,
         child: Container(
           padding: EdgeInsets.all(12),
-          child: Text(
-            widget.values[int.parse(widget.controller.text)],
-            textAlign: TextAlign.center,
-            style: Theme.of(context).textTheme.display1,
+          child: AnimatedBuilder(
+            animation: controller,
+            builder: (_, __) => Text(
+              values[int.parse(controller.text)],
+              textAlign: TextAlign.center,
+              style: Theme.of(context).textTheme.display1,
+            ),
           ),
         ),
       );
 
   void _onTap() {
-    var oldValue = int.parse(widget.controller.text);
-    var newValue = (oldValue + 1) % (widget.values.length);
-    widget.controller.text = newValue.toString();
-    widget.onChanged();
-    setState(() {});
+    var oldValue = int.parse(controller.text);
+    var newValue = (oldValue + 1) % (values.length);
+    controller.text = newValue.toString();
+    onChanged();
   }
 }
